1926f2e84fe9aba8c6bcc70a74644b191565504f
Daniel Hollas <danekhollas@gmail.com>
2022-03-09 14:50:28 +0000

    WIP: Add eshift to restart file to preserve continuity

--- src/sh_integ.F90
+++ src/sh_integ.F90
@@ -453,6 +453,7 @@ contains
    subroutine sh_write_wf(outunit)
       integer, intent(in) :: outunit
       integer :: ist1
+      write (outunit, *) eshift
       do ist1 = 1, nstate
          write (outunit, *) cel_re(ist1), cel_im(ist1)
       end do
@@ -466,6 +467,7 @@ contains
          allocate (cel_re(nstate), cel_im(nstate))
       end if
 
+      read (inunit, *) eshift
       do ist1 = 1, nstate
          read (inunit, *) cel_re(ist1), cel_im(ist1)
       end do
