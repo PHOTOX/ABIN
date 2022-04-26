d2bcd182caf14ac205be95c33f7c7d668864fd8f
Daniel Hollas <danekhollas@gmail.com>
2022-04-26 22:15:47 +0200

    Test that ABIN doesn't ovewrite movie/restart files

---
+++ tests/INIT/test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -euo pipefail
+
+rm -f ERROR abin.out movie.xyz restart.xyz *dat
+if [[ $1 = "clean" ]];then
+  exit 0
+fi
+
+ABINEXE=$1
+
+# Testing that ABIN does not run
+# if movie.xyz and/or restart.xyz are present
+# and irest=0
+
+touch movie.xyz
+$ABINEXE -x mini.xyz -i input.in > abin.out 2>&1 || true
+mv ERROR ABIN_ERROR1
+
+rm movie.xyz
+touch restart.xyz
+$ABINEXE -x mini.xyz -i input.in > abin.out 2>&1 || true
+mv ERROR ABIN_ERROR2
