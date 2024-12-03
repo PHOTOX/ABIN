1cf13d110ada92c9661993822c93ab65930177e6
Jiri Janos <jiri.janos98@gmail.com>
2024-12-03 16:38:36 +0100

    open nacme_all.dat only if couplings='analytic'

--- src/surfacehop.F90
+++ src/surfacehop.F90
@@ -177,8 +177,8 @@ contains
       dum_fx = 0.0D0; dum_fy = 0.0D0; dum_fz = 0.0D0
       call force_clas(dum_fx, dum_fy, dum_fz, x, y, z, dum_eclas, pot)
 
-      ! open nacme_all.dat for all but baeck-an couplings
-      if (inac /= 1) call nacmefile_init()
+      ! open nacme_all.dat if NACME is calculated
+      if (inac == 0) call nacmefile_init()
 
       ! When restarting, initial SH WF was already read from the restart file
       if (irest == 0) then
